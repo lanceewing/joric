3ed58312f45f8e433bb994a42fa2fef778e338fa
Lance Ewing <lance.ewing@gmail.com>
2025-01-03 00:03:09 +0000

    Changed desktop to also load from URLs rather than internal assets.

--- lwjgl3/src/main/java/emu/joric/lwjgl3/DesktopProgramLoader.java
+++ lwjgl3/src/main/java/emu/joric/lwjgl3/DesktopProgramLoader.java
@@ -1,9 +1,10 @@
 package emu.joric.lwjgl3;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.function.Consumer;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 
 import emu.joric.PixelData;
 import emu.joric.Program;
@@ -19,10 +20,30 @@ public class DesktopProgramLoader implements ProgramLoader {
     @Override
     public void fetchProgram(AppConfigItem appConfigItem, Consumer<Program> programConsumer) {
         Program program = null;
-        FileHandle fileHandle = Gdx.files.internal(appConfigItem.getFilePath());
-        if (fileHandle != null) {
-            if (fileHandle.exists()) {
-                program = new Program(appConfigItem, fileHandle.readBytes());
+        BufferedInputStream bis = null;
+        
+        try {
+            URL url = new URL(appConfigItem.getFilePath());
+            URLConnection connection = url.openConnection();
+            
+            int b = 0;
+            bis = new BufferedInputStream(connection.getInputStream());
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            while ((b = bis.read()) != -1 ) {
+                out.write(b);
+            }
+            
+            program = new Program(appConfigItem, out.toByteArray());
+            
+        } catch (Exception e) {
+            // Ignore.
+        } finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (Exception e2) {
+                 // Ignore.
+                }
             }
         }
         
